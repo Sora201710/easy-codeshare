453423f34da8d3de1c00c6131109af45f6158b59
Christopher Roy <chrisroybd@gmail.com>
2026-03-11 00:32:48 -0400

    judge0 works with backend python language but not C

--- backend/judge_demo.py
+++ backend/judge_demo.py
@@ -1,19 +1,21 @@
 import judge0
+import requests
 from dotenv import load_dotenv
 import os
 
-load_dotenv();
-
+load_dotenv()
 
 source_code = """
 #include <stdio.h>
 
 int main() {
-    printf("hello, world in  c\\n");
+    printf("hello, world in  c\n");
     return 0;
 }
 """
+result = requests.post(
+    "http://localhost:2358/submissions",
+    data={"source_code": source_code, "language_id": judge0.PYTHON},
+)
 
-result = judge0.run(source_code=source_code, language=judge0.C)
-
-print(result.stdout)
+print(result.json())
